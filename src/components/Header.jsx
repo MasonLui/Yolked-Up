b8ce22dfce78636733c80c056c3692503feac27a
paigei <113061249+paigei@users.noreply.github.com>
2024-12-04 21:09:11 -0800

    moved images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import yolkedup from '../yolkedup.jpg';
+import yolkedupCopy from '../yolkedup copy.jpg';
  Header.jsx
 export default function Header() {
   return (
     <header>
-      <img src="yolkedup.jpg" />
-      Yolked-Up
+      <img src={yolkedup} alt="egg" />
+      <h1>Yolked-Up</h1>
+      <img src="yolkedup copy.jpg" alt="egg" />
     </header>
   );
 }
